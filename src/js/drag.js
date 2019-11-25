56943736c44e1e867ea8c8665ee0b1db4ffc2649
Oskin <wskeal@gmail.com>
2019-11-25 16:17:40 +0300

    update dnd styling & logic

--- src/js/drag.js
+++ src/js/drag.js
@@ -9,30 +9,36 @@ function onDragStart(e) {
 
 function onDragEnd(e) {
     e.target.style.opacity = "";
+    e.target.style.background = "#fff";
 }
 
 function onDragEnter(e) {
     if (e.target.classList.contains("card")) {
-        e.target.style.opacity = .5;
+        e.target.style.background = "#ababab";
     }
 }
 
 function onDragLeave(e) {
     if (e.target.classList.contains("card")) {
-        e.target.style.opacity = "";
+        e.target.style.background = "#fff";
     }
 }
 
 function onDrop(e) {
     e.preventDefault();
     if (e.target.classList.contains("card") && e.target !== draggableCard) {
-        draggableCard.parentNode.removeChild(draggableCard);
         e.target.style.opacity = "";
+        e.target.style.background = "#fff";        
         appendAfter(e.target, draggableCard);
+
     }
 }
 
 function appendAfter(element, newElement) {
+    if (draggableCard.previousSibling === element) {
+        element.parentNode.insertBefore(newElement, element);
+        return;
+    }
     element.parentNode.insertBefore(newElement, element.nextSibling);
 }
 
@@ -41,7 +47,7 @@ const initDND = () => {
     document.addEventListener("dragend", onDragEnd, false);
     document.addEventListener("dragenter", onDragEnter, false);
     document.addEventListener("dragleave", onDragLeave, false);
-    document.addEventListener("dragover", function (e) {e.preventDefault()}, false);
+    document.addEventListener("dragover", function (e) { e.preventDefault() }, false);
     document.addEventListener("drop", onDrop, false);
 }
 
